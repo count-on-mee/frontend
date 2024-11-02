ace0a45d742eabf7449b9333a85eeca93308345c
chaerikim <curry4861@naver.com>
2024-11-02 14:03:12 +0900

    [Feat]SpotPage 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import CalendarPage from './pages/CalendarPage';
 import DestinationListPage from './pages/DestinationListPage';
+import SpotPage from './pages/SpotPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/calendar" element={<CalendarPage />} />
+      <Route path="/spot" element={<SpotPage />} />
       <Route path="/destination" element={<DestinationListPage />} />
     </Routes>
   );
