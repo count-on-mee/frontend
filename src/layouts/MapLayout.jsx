65d88e4fb2fe24022a5baf5a9787141555639185
insoo00 <insoosos1234@gmail.com>
2024-11-05 20:26:39 +0900

    [Feat] 화면 사이즈에 맞춰 지도 크기 조정

--- src/layouts/MapLayout.jsx
+++ src/layouts/MapLayout.jsx
@@ -14,22 +14,24 @@ export default function MapLayout() {
       <Header />
       <Outlet /> {/* 자식 컴포넌트가 렌더링될 위치 */}
       {/* 변경되지 않을 컴포넌트 */}
-      <MapDiv
-        className="overflow-hidden"
-        style={{
-          width: '100%',
-          height: '680px',
-        }}
-      >
-        <NaverMap
-          defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
-          defaultZoom={15}
+      <div className="w-full h-svh">
+        <MapDiv
+          className="overflow-hidden"
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
         >
-          <Marker
-            defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)}
-          />
-        </NaverMap>{' '}
-      </MapDiv>
+          <NaverMap
+            defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
+            defaultZoom={15}
+          >
+            <Marker
+              defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)}
+            />
+          </NaverMap>{' '}
+        </MapDiv>
+      </div>
     </div>
   );
 }
