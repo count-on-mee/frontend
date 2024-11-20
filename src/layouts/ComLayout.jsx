1ac7fdc12e0c9d0602c47a61e0479c37af2aa183
lehaeiin <{alkane0410@naver.com}>
2024-11-20 13:05:49 +0900

    [Feat] map 불러오는 방식 수정 및 이중 아울렛 수정

--- src/layouts/ComLayout.jsx
+++ src/layouts/ComLayout.jsx
@@ -4,7 +4,12 @@ import COMNavbar from '../components/COMNavbar';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ErrorBoundary from '../components/ErrorBoundary';
-const Map = React.lazy(() => import('../layouts/MapLayout'));
+import {
+  Container as MapDiv,
+  NaverMap,
+  Marker,
+  useNavermaps,
+} from 'react-naver-maps';
 import CalendarPage from '../pages/CalendarPage';
 import DestinationListPage from '../pages/DestinationListPage';
 import MyScrapListPage from '../pages/MyScrapListPage';
@@ -12,6 +17,7 @@ import MyScrapListPage from '../pages/MyScrapListPage';
 const COMLayout = () => {
   const [scrollY, setScrollY] = useState(0);
   const location = useLocation();
+  const navermaps = useNavermaps();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -38,69 +44,78 @@ const COMLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <COMNavbar />
       <div className="flex-grow bg-[#FFFCF2] flex">
-        <div className="w-1/2 pr-2">
-          <div className="bg-[#FFFCF2] w-full">
-            <COMNavbar />
-            <ErrorBoundary>
-              {isPopupRoute ? (
-                <div className="fixed inset-0 z-50 overflow-hidden">
-                  <div className="flex items-center justify-center min-h-screen p-4">
-                    <div
-                      className="fixed inset-0 transition-opacity"
-                      aria-hidden="true"
-                    >
-                      <div className="absolute inset-0 bg-[#FFFCF2] opacity-10 backdrop-filter backdrop-blur-xl"></div>
-                    </div>
-                    <div className="bg-white rounded-lg shadow-xl w-full max-w-md relative z-10 flex flex-col">
-                      <div className="flex-grow overflow-hidden">
-                        <Suspense fallback={<div>Loading...</div>}>
-                          {/* 팝업으로 띄울 내용 */}
-                          {location.pathname === '/com/calendar' && (
-                            <CalendarPage />
-                          )}
-                          {location.pathname === '/com/destinationlist' && (
-                            <DestinationListPage />
-                          )}
-                          {location.pathname === '/com/myscraplist' && (
-                            <MyScrapListPage />
-                          )}
-                        </Suspense>
-                      </div>
-                    </div>
+        <ErrorBoundary>
+          {isPopupRoute ? (
+            <div className="fixed inset-0 z-50 overflow-hidden">
+              <div className="flex items-center justify-center min-h-screen p-4">
+                <div
+                  className="fixed inset-0 transition-opacity"
+                  aria-hidden="true"
+                >
+                  <div className="absolute inset-0 bg-[#FFFCF2] opacity-10 backdrop-filter backdrop-blur-xl"></div>
+                </div>
+                <div className="bg-white rounded-lg shadow-xl w-full max-w-md relative z-10 flex flex-col">
+                  <div className="flex-grow overflow-hidden">
+                    <Suspense fallback={<div>Loading...</div>}>
+                      {location.pathname === '/com/calendar' && (
+                        <CalendarPage />
+                      )}
+                      {location.pathname === '/com/destination-list' && (
+                        <DestinationListPage />
+                      )}
+                      {location.pathname === '/com/my-scrap-list' && (
+                        <MyScrapListPage />
+                      )}
+                    </Suspense>
                   </div>
                 </div>
-              ) : (
+              </div>
+            </div>
+          ) : (
+            <div className="w-full flex">
+              <div className="w-1/2 pr-2">
                 <Suspense fallback={<div>Loading...</div>}>
-                  {isOutletRoute ? (
-                    <>
-                      <COMNavbar />
-                      <Outlet />{' '}
-                    </>
-                  ) : (
-                    ' '
-                  )}
+                  {isOutletRoute && <Outlet />}
                 </Suspense>
+              </div>
+              {isOutletRoute && (
+                <div className="w-1/2 pl-2 p-1 relative">
+                  <div
+                    className="sticky"
+                    style={{
+                      top: `${Math.max(0, scrollY)}px`,
+                      height: '80vh',
+                      transition: 'top 0.1s ease-out',
+                    }}
+                  >
+                    <ErrorBoundary>
+                      <Suspense fallback={<div>Loading map...</div>}>
+                        <MapDiv style={{ width: '100%', height: '100%' }}>
+                          {navermaps && (
+                            <NaverMap
+                              defaultCenter={
+                                new navermaps.LatLng(37.3595704, 127.105399)
+                              }
+                              defaultZoom={15}
+                            >
+                              <Marker
+                                position={
+                                  new navermaps.LatLng(37.3595704, 127.105399)
+                                }
+                              />
+                            </NaverMap>
+                          )}
+                        </MapDiv>
+                      </Suspense>
+                    </ErrorBoundary>
+                  </div>
+                </div>
               )}
-            </ErrorBoundary>
-          </div>
-        </div>
-        <div className="w-1/2 pl-2 p-7 relative">
-          <div
-            className="sticky"
-            style={{
-              top: `${Math.max(0, scrollY)}px`,
-              height: '80vh',
-              transition: 'top 0.1s ease-out',
-            }}
-          >
-            <ErrorBoundary>
-              <Suspense fallback={<div>Loading map...</div>}>
-                <Map />
-              </Suspense>
-            </ErrorBoundary>
-          </div>
-        </div>
+            </div>
+          )}
+        </ErrorBoundary>
       </div>
       <Footer />
     </div>
