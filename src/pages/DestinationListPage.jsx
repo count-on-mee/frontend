b191e90242c1241550a12e41f09c99d83fa88001
lehaeiin <{alkane0410@naver.com}>
2024-11-20 13:05:01 +0900

    [Refactor] 잘못된 컴포넌트 수정

--- src/pages/DestinationListPage.jsx
+++ src/pages/DestinationListPage.jsx
@@ -1,6 +1,6 @@
 import DestinationList from '../components/DestinationList';
 
-export default function CalendarPage() {
+export default function DestinationListPage() {
   return (
     <div className="bg-[#FFFCF2] w-full max-w-4xl h-[80vh] overflow-auto">
       <DestinationList />
