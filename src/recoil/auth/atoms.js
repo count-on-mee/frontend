62fcf2cf1a5ea02c6264809de451238266373b7a
chaerikim <curry4861@naver.com>
2024-12-12 22:08:28 +0900

    [Feat] authatom수정

--- src/recoil/auth/atoms.js
+++ src/recoil/auth/atoms.js
@@ -2,36 +2,11 @@ import { atom } from 'recoil';
 
 const authAtom = atom({
   key: 'auth',
-  default: { isAuthenticated: false },
-  effects: [
-    ({ setSelf, onSet }) => {
-        const savedValue = localStorage.getItem('auth');
-        if (savedValue != null) {
-          setSelf(JSON.parse(savedValue));
-        }
-        onSet((newValue) => {
-          console.log('onSet called with:', newValue);
-          localStorage.setItem('auth', JSON.stringify(newValue));
-        })
-    },
-  ],
-});
+  default: { 
+    isAuthenticated: false,
+    token: null,
+ },
 
-// const authAtom = atom({
-//   key: 'auth',
-//   default: {isAuthenticated: false},
-//   effects: [
-//     ({ setSelf, onSet }) => {
-//         const savedData = localStorage.getItem("auth");
-//         if (savedData) setSelf(JSON.parse(savedData));
-//         onSet((newValue, isReset) => {
-//           console.log('onSet called with:', newValue, isReset);
-//           isReset
-//             ? localStorage.removeItem("auth")
-//             : localStorage.setItem("auth", JSON.stringify(newValue));
-//         })
-//     },
-//   ],
-// });
+});
 
 export default authAtom;
