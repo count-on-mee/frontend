1297af7f5bac319fd17955d5863732f7eca9e542
chaerikim <curry4861@naver.com>
2025-01-02 23:53:50 +0900

    [Fix] MyPage수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,20 +1,121 @@
 import Footer from '../components/Footer';
-import { useState } from 'react';
-import { PencilSquareIcon, PlusCircleIcon, UserIcon, CheckIcon } from '@heroicons/react/24/outline';
+import { useState, useEffect } from 'react';
+import { PencilSquareIcon, CheckIcon, BookmarkIcon } from '@heroicons/react/24/outline';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import userAtom from '../recoil/user';
 import ProfileImage from '../components/ProfileImage';
 import UploadImages from '../components/UploadImages';
 import { updateUser } from '../services/userService';
+import selectedSpotAtom from '../recoil/selectedSpot';
 
 
 export default function MyPage () {  
   const user = useRecoilValue(userAtom);
   const setUser = useSetRecoilState(userAtom);
+  const setSelectedSpot = useSetRecoilState(selectedSpotAtom);
 
   const [nickname, setNickname] = useState(user?.nickname);
   const [isEditing, setIsEditing] = useState(false);
   const [error, setError] = useState('');
+  
+  const [scrapedSpots, setScrapedSpots] = useState([]);
+  const [scrapedCurations, setScrapedCurations] = useState([]);
+
+
+  const fetchScrapedSpots = async () => {
+    try {
+      const token = localStorage.getItem('accessToken');
+      const response = await fetch('http://localhost:8888/scraps/spots', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setScrapedSpots(data);
+    } catch (error) {
+      console.error('Failed to fetch scraped spots:', error);
+    }
+  };
+
+  const fetchScrapedCurations = async () => {
+    try {
+      const token = localStorage.getItem('accessToken');
+      const response = await fetch('http://localhost:8888/scraps/curations', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setScrapedCurations(data);
+    } catch (error) {
+      console.error('Failed to fetch scraped curations:', error);
+    }
+  };
+
+  useEffect(() => {
+      fetchScrapedSpots();
+      fetchScrapedCurations();
+    }, []);
+
+    const handleSpotScrap = async ( event, spotId ) => {
+      event.stopPropagation();
+      try {
+        const token = localStorage.getItem('accessToken');
+        const response = await fetch(`http://localhost:8888/scraps/spots/${spotId}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          body: JSON.stringify({ isDeleted: true })
+        });
+        if (!response.ok) {
+          throw new Error('Failed to cancel scrap');
+        }
+        fetchScrapedSpots();
+      } catch (error) {
+        console.error('스크랩 취소 에러:', error);
+      }
+    }
+
+    const handleCurationScrap = async ( event, curationId ) => {
+      event.stopPropagation();
+      try {
+        const token = localStorage.getItem('accessToken');
+        const response = await fetch(`http://localhost:8888/scraps/curations/${curationId}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          body: JSON.stringify({ isDeleted: true })
+        });
+        if (!response.ok) {
+          throw new Error('Failed to cancel scrap');
+        }
+        fetchScrapedCurations();
+      } catch (error) {
+        console.error('스크랩 취소 에러:', error);
+      }
+    }
+  
+  // const handleSpotNavigate = (spot) => {
+  //   setSelectedSpot(spot);
+  //   console.log(selectedSpotAtom);
+  //   navigate('/map/spot');
+  // }
 
   const validateNickname = () => {
     if (nickname.length > 30){
@@ -47,8 +148,8 @@ export default function MyPage () {
   const handleImageChange = (uploadImage) => { 
     if (uploadImage) {
     const uploadImageUrl = URL.createObjectURL(uploadImage[0]);
-    setUser((prevUSer) => ({
-      ...prevUSer,
+    setUser((prevUser) => ({
+      ...prevUser,
       profileImgUrl: uploadImageUrl,
     }))
     updateUser({ uploadImage: uploadImage[0]});
@@ -58,7 +159,7 @@ export default function MyPage () {
   return(
     <div className="w-full flex flex-col bg-[#FFFCF2]">
       <div className="flex flex-nowrap flex-grow">
-        <div className="font-mixed w-1/4 border-[#403D39] border-r-2 text-center text-2xl pt-5">
+        <div className="font-mixed box-content w-1/4 border-[#403D39] border-r-2 text-center text-2xl pt-5">
           My page
             <div className="relative">
               <div className="w-1/2 h-1/2 mx-auto">
@@ -100,13 +201,35 @@ export default function MyPage () {
                   )}
             </div>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 w-3/4">
             <div className="inline">
               <div 
                 className="inline-block px-3 py-2 mt-5 ml-10 font-mixed rounded-tl-lg rounded-br-lg bg-[#CCC5B9]">
                 spot      
               </div>
-              <div className="h-1 bg-black rounded-lg mx-10 mb-52"></div>
+              <div className="h-1 bg-black rounded-lg mx-10"></div>
+              {scrapedSpots.length > 0 && (
+                <div className="flex h-52 mx-10 overflow-x-auto">
+                {scrapedSpots.map(spot => (
+                  <div 
+                    key={spot.spotScrapId} onClick={() => handleSpotNavigate(spot)} className="mx-3 cursor-pointer"
+                  >
+                    <div className="relative">
+                      <img 
+                        src={spot.imgUrl}
+                        alt={spot.title}
+                        className="flex relative h-36 w-60 rounded-md mt-2"
+                      />
+                      <BookmarkIcon
+                        className={`absolute top-3 right-3 w-5 h-5 ${spot.isDeleted ? '' : 'fill-[#EB5E28] stroke-[#EB5E28]'}`} onClick={(event) => handleSpotScrap(event, spot.spotId)}
+                      />
+                    </div>
+                    <div className="font-mixed truncate w-56 font-light text-md">{spot.title}</div>
+
+                  </div>  
+                ))}
+              </div>
+              )}
             </div>
             <div className="inline">
               <div 
@@ -114,6 +237,27 @@ export default function MyPage () {
                 curation     
               </div>
               <div className="h-1 bg-black rounded-lg mx-10"></div>
+              {scrapedCurations.length > 0 && (
+                <div className="flex h-72 mx-10 overflow-x-auto">
+                {scrapedCurations.map(curation => (
+                  <div key={curation.curationScrapId} className="mx-3 cursor-pointer w-40">
+                    <div className="relative w-40">
+                      <img 
+                        src={curation.imgUrl}
+                        alt={curation.title}
+                        className="flex h-60 w-40 rounded-md mt-2 object-cover"
+                      />
+                      <BookmarkIcon
+                        className={`absolute top-3 right-3 w-5 h-5 ${curation.isDeleted ? '' : 'fill-[#EB5E28] stroke-[#EB5E28]'}`} onClick={(event) => handleCurationScrap(event, curation.curationId)}
+                      />
+                      <div className="absolute font-mixed text-white truncate w-40 left-1 bottom-1 text-md font-bold">{curation.title}</div>
+                    </div>
+                    
+
+                  </div>  
+                ))}
+              </div>
+              )}
             </div>
           </div>
         </div>
