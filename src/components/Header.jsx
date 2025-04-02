d2f05b7352b6bc6501e6ba6c452054a13e08fcf3
chaerikim <curry4861@naver.com>
2025-04-02 20:56:36 +0900

    ✨ Tailwindcss v4
    tailwindcss v4적용

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="bg-[#EB5E28]">
+      <Link to="/">header</Link>
+    </header>
+  );
+}
