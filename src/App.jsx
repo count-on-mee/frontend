1ddccf275c2ad1b03a22c74bdbbb05873e5fdabc
lehaeiin <{alkane0410@naver.com}>
2025-04-07 15:04:12 +0900

    ✨ Calendar 개발완료 #20
    src/Layouts/PlanLayout.jsx, src/components/plan/Calendar.jsx및 recoil/currentMonth, recoil/tripDates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+//import InitializeUserWrapper from './components/InitializeUserWrapper';
+import PlanLayout from './layouts/PlanLayout';
+import Calendar from './components/plan/Calendar';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h2>안녕!</h2>
-    </Router>
+    <React.StrictMode>
+      <RecoilRoot>
+        {/* <InitializeUserWrapper /> */}
+        <div className="flex flex-col min-h-screen">
+          {/* <Header /> */}
+          <main className="flex-grow flex">
+            <Routes>
+              <Route path="/com" element={<PlanLayout />}>
+                <Route path="calendar" element={<Calendar />} />
+              </Route>
+            </Routes>
+          </main>
+        </div>
+      </RecoilRoot>
+    </React.StrictMode>
   );
 }
 
