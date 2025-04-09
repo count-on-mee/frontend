9c7bdc4f36ac28aa803c51983dc09fee7a29178c
chaerikim <curry4861@naver.com>
2025-04-09 16:31:23 +0900

    ✨ Header구현 #23
    기본적인 것만 구현했습니다. 로그인 기능 추가시에 수정하겠습니다.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { RecoilRoot } from 'recoil';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
